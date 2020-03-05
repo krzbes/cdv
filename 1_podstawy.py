9438da6cf8fdf7ef85b5066b75a26e9b74c302cf
Krzysztof Besser <kbesser@edu.cdv.pl>
2020-03-05 17:21:10 +0100

    first lesson

--- 1_podstawy.py
+++ 1_podstawy.py
@@ -1,1 +1,39 @@
-pritn("janusz")
+
+'''
+# potęgowanie
+potega = 2**10
+print(potega)
+
+# pobieranie danych z klawiatury
+name = input()
+surname = "Nowak"
+print("nazywasz się "+name+" "+surname)
+
+print("Podaj swój wiek: ",end =" ")
+age = input()
+
+age1=20
+print(type(age1))
+print("twój wiek: ",age1," lata")
+'''
+#
+surname="Kowalski"
+firstChar=surname[0]
+print(firstChar)
+
+lenght = len(surname)
+print(lenght)
+lastChar = surname[lenght-1]
+print(lastChar)
+print(surname[-1])
+
+#konwersja
+x = "5"
+print(type(x))#str
+x = int(x)
+print(type(x))#int
+
+y=1
+print(type(y))#int
+y=y/2
+print(type(y))#float
